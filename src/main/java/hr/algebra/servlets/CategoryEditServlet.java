d2fc517aeb01bd64fd83ca9fb1f61cd03ddc6108
josipsha <josip.sadek1998@gmail.com>
2022-12-31 11:56:35 +0100

    -Created Admin servlet and jsp -Configured authorized endpoints

---
+++ src/main/java/hr/algebra/servlets/CategoryEditServlet.java
@@ -0,0 +1,30 @@
+package hr.algebra.servlets;
+
+import hr.algebra.data.IUnitOfWork;
+import hr.algebra.data.RepoFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CategoryEditServlet", value = "/edit/category")
+public class CategoryEditServlet extends HttpServlet {
+    private IUnitOfWork uow;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        uow = RepoFactory.getUnitOfWork();
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        res.setContentType("text/html");
+
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/Pages/CategoryEditPage.jsp");
+        requestDispatcher.forward(req, res);
+    }
+}
---
+++ src/main/java/hr/algebra/servlets/LoginHistoryServlet.java
@@ -0,0 +1,30 @@
+package hr.algebra.servlets;
+
+import hr.algebra.data.IUnitOfWork;
+import hr.algebra.data.RepoFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginHistoryServlet", value = "/login-history")
+public class LoginHistoryServlet extends HttpServlet {
+    private IUnitOfWork uow;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        uow = RepoFactory.getUnitOfWork();
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        res.setContentType("text/html");
+
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("Pages/LoginHistoryPage.jsp");
+        requestDispatcher.forward(req, res);
+    }
+}
